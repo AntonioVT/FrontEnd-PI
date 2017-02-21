d197293fcbf74fb24b12aa84a6121a1083e074bc
Antonio Vargas <tony-vt@hotmail.com>
2017-02-21 13:05:59 -0600

    avance spinner

--- js/global.js
+++ js/global.js
@@ -12,6 +12,27 @@ $(document).ready(function() {
 	}
 });
 
+var loadingSpinner, changePasswordForm;
+
+function getProfileReferences () {
+	loadingSpinner = document.getElementById('loadingSpinner');
+	changePasswordForm = document.getElementById('changePasswordForm');
+	loadingSpinner.style.display = 'none';
+}
+
+
+function changePassword () {
+	var saveChildren = changePasswordForm.innerHTML;
+	var saveHeight = changePasswordForm.style.height
+	changePasswordForm.innerHTML = '';
+	changePasswordForm.style.opacity = '1';
+	changePasswordForm.style.height = '20px';
+	changePasswordForm.style.position = 'inherit';
+	loadingSpinner.style.display = 'block';
+	console.log(saveChildren);
+	console.log(saveHeight);
+}
+
 function updateCharactersLeft (textarea) {
 	var charactersMessage = textarea.parentElement.getElementsByTagName("p")[0];
 	var actualCharacters = textarea.value.length;
