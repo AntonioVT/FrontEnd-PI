a6e3110ee71a08f003ea825563b6abdd9abaa96f
Antonio Vargas <tony-vt@hotmail.com>
2017-02-22 14:04:13 -0600

    avance checkmark animacion cambiar contraseña

--- js/global.js
+++ js/global.js
@@ -12,25 +12,58 @@ $(document).ready(function() {
 	}
 });
 
-var loadingSpinner, changePasswordForm;
+var loadingSpinner, changePasswordForm, passwordCheckMark;
 
 function getProfileReferences () {
 	loadingSpinner = document.getElementById('loadingSpinner');
 	changePasswordForm = document.getElementById('changePasswordForm');
+	passwordCheckMark = document.getElementById('passwordCheckMark');
 	loadingSpinner.style.display = 'none';
+	passwordCheckMark.style.display = 'none';
 }
 
-
 function changePassword () {
-	var saveChildren = changePasswordForm.innerHTML;
-	var saveHeight = changePasswordForm.style.height
-	changePasswordForm.innerHTML = '';
-	changePasswordForm.style.opacity = '1';
-	changePasswordForm.style.height = '20px';
-	changePasswordForm.style.position = 'inherit';
 	loadingSpinner.style.display = 'block';
-	console.log(saveChildren);
-	console.log(saveHeight);
+
+	var saveChildren = changePasswordForm.innerHTML;
+	var saveHeight = changePasswordForm.clientHeight;
+	var spinnerHeight = loadingSpinner.clientHeight;
+
+	var normalizedHeight = saveHeight - spinnerHeight - 48;
+	var expectedHeight = 0;
+
+	changePasswordForm.style.height = normalizedHeight + 'px';
+	loadingSpinner.style.opacity = '0%';
+
+	// transitions
+	changePasswordForm.style.transition = "height .40s ease-in";
+	loadingSpinner.style.transition = 'opacity .40s ease-in';
+
+	// cambios
+	changePasswordForm.innerHTML = '';
+	//changePasswordForm.style.opacity = '1';
+	changePasswordForm.style.height = expectedHeight + 'px';
+	loadingSpinner.style.opacity = '1';
+	passwordCheckMark.style.opacity = '1';
+
+	// Horizontal
+	setTimeout(function(){
+		changePasswordForm.style.transition = 'width .40s ease-in, height .20s ease-in, background-color .20s ease-in, box-shadow .20s ease-in';
+		// loadingSpinner.style.opacity = '100%';
+	}, 250);
+
+	setTimeout(function () {
+		console.log('Done')
+		loadingSpinner.style.opacity = '0';
+		loadingSpinner.style.transition = 'opacity .40s ease-in';
+	}, 1250);
+
+	setTimeout(function () {
+		//loadingSpinner.remove();
+		passwordCheckMark.style.transition = 'opacity .40s ease-in';
+		passwordCheckMark.style.display = 'block';
+	}, 1300);
+
 }
 
 function updateCharactersLeft (textarea) {
